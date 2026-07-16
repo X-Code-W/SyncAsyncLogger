79483930f7996a3c8c98af8c0bfb68565fd24004
X-代码-W <758497189@qq.com>
2026-07-16 21:35:07 +0800

    日志等级类设计

---
+++ logs/level.hpp
@@ -0,0 +1,34 @@
+
+#ifndef __LEVEL_HPP_
+#define __LEVEL_HPP_
+namespace mylog
+{
+    class LogLevel
+    {
+        public:
+            enum class value
+            {
+                UNKNOW=0,
+                DEBUG,
+                INFO,
+                WARN,
+                ERROR,
+                FATAL,
+                OFF    
+            };
+            static const char* ToString(LogLevel::value level)
+            {
+                switch (level)
+                {
+                    case LogLevel::value::DEBUG: return "DEBUG";
+                    case LogLevel::value::INFO: return "INFO";
+                    case LogLevel::value::WARN: return "WARN";
+                    case LogLevel::value::ERROR: return "ERROR";
+                    case LogLevel::value::FATAL: return "FATAL";
+                    case LogLevel::value::OFF: return "OFF";
+                }
+                return "UNKNOW";
+            }
+    };
+}
+#endif
